70307b68200116a0a2403609d115ce213312f92e
Angel <Angel@DESKTOP-S65TM8B>
2021-03-28 20:53:17 +0800

    second commit

--- testwebservice-1/src/main/java/com/example/myfirstwebservice/controller/GreetingRest.java
+++ testwebservice-1/src/main/java/com/example/myfirstwebservice/controller/GreetingRest.java
@@ -1,6 +1,7 @@
 package com.example.myfirstwebservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +16,14 @@ import com.example.myfirstwebservice.service.GreetingService;
 public class GreetingRest {
 	@Autowired
 	GreetingService service;
+	
+	@Value("${custom.config.key}")
+	float key;
 
 	
 	@GetMapping("/greet")
 	public String greetByPassingParam(@RequestParam int i, @RequestParam int v, @RequestParam int a, @RequestParam int n) {		
-		return service.viewGreeting((i + v + a + n)*20.42);
+		return service.viewGreeting((i + v + a + n)*key);
 	}
 	
 
